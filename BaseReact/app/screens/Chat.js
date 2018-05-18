0be1718831a678163f39b183878dd0f5c455afdd
geoninh <ninhnm91@gmail.com>
2018-05-18 15:03:27 +0700

    add socket

--- BaseReact/app/screens/Chat.js
+++ BaseReact/app/screens/Chat.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import io from 'socket.io-client/dist/socket.io.js';
 
 export default class Chat extends Component {
+    constructor(props){
+        super(props);
+        this.socket = io ("http://192.168.3.126:3000", {jsonp:false});
+    }
+
     render() {
         return (
             <Text> aaaa </Text>
